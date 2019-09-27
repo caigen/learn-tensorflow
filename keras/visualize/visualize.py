4ccc0c59417b108506b16cb9c3c2d25fa18f9f04
caigen <caigen@outlook.com>
2019-09-27 14:34:00 +0800

    plot_model

---
+++ keras/visualize/visualize.py
@@ -0,0 +1,14 @@
+"""
+ref:
+https://machinelearningmastery.com/visualize-deep-learning-neural-network-model-keras/
+"""
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils.vis_utils import plot_model
+
+model = Sequential()
+model.add(Dense(2, input_dim=1, activation="relu"))
+model.add(Dense(1, activation="sigmoid"))
+
+print(model.summary())
+plot_model(model, to_file="model.png", show_shapes=True, show_layer_names=True)
