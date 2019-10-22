08759c2ac6f43c030b4dcaf5a552b2e8e265b50f
caigen <caigen@outlook.com>
2019-10-22 11:11:26 +0800

    activation layer demo.

--- keras/visualize/visualize.py
+++ keras/visualize/visualize.py
@@ -4,9 +4,11 @@ https://machinelearningmastery.com/visualize-deep-learning-neural-network-model-
 """
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Activation
 from keras.utils.vis_utils import plot_model
 
 model = Sequential()
+model.add(Activation(activation="relu", input_shape=[1]))
 model.add(Dense(2, input_dim=1, activation="relu"))
 model.add(Dense(1, activation="sigmoid"))
 
